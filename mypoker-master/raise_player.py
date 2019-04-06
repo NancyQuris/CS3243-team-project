76e0290a53c5c0b0cf9ec39ca9424504c39ded88
KongZijin <e0253754@u.nus.edu>
2019-04-06 13:24:24 +0800

    add raise round constrain

--- mypoker-master/raise_player.py
+++ mypoker-master/raise_player.py
@@ -1,10 +1,35 @@
 from pypokerengine.players import BasePokerPlayer
 from time import sleep
 import pprint
+import collections
 
 class RaisedPlayer(BasePokerPlayer):
 
   def declare_action(self, valid_actions, hole_card, round_state):
+    # check if raise 4 times alr, cannot raise any more
+
+  # round_state
+    pp = pprint.PrettyPrinter(indent=2)
+
+    flag = True
+    current_round = round_state['action_histories'][round_state['street']]
+    uuid1 = round_state['seats'][0]['uuid']
+    uuid2 = round_state['seats'][1]['uuid']
+
+  # (current_round)
+    raiseCount = collections.defaultdict(int)
+
+    for action_details in current_round:
+        if action_details['action'] is 'RAISE' or 'BIGBLIND':
+            # Big blind is also considered as 'RAISE'
+            raiseCount[action_details['uuid']] += 1
+
+    if raiseCount[uuid1] >= 4 or raiseCount[uuid2] >= 4:
+        flag = False
+
+    pp.pprint("ROUND STATE")
+    pp.pprint(round_state)
+
     for i in valid_actions:
         if i["action"] == "raise":
             action = i["action"]
